5ad68890f10d2d45cd9059074e3a793299d3c63f
Bartosz Bak <bartek.bak@protonmail.com>
2026-05-05 10:53:10 +0100

    feat: add batch normalisation to stabilise training

--- model.py
+++ model.py
@@ -13,15 +13,19 @@ class PetBreedConvolutionalNetwork(nn.Module):
         # these layers learn image features from the input pixels
         self.convolution_layers = nn.Sequential(
             nn.Conv2d(3, 16, kernel_size=3, padding=1),
+            nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
             nn.Conv2d(16, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
             nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
         )
